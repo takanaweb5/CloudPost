d7e47ef0689879dea13e799ec8779ce8a43a6a8e
takahasinaoki@gmail.com <takanaweb5>
2021-02-05 23:28:06 +0900

    いろいろ

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,11 @@
 import * as functions from "firebase-functions";
+import * as axios from "axios";
+import * as express from "express";
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const helloWorld = functions.region("asia-northeast2").https.onRequest((req: express.Request, res: express.Response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+    res.send("Hello from Firebase!");
+});
