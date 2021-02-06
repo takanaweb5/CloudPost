f7a2aa8d0a0e327cfcc7ac502c3556df54df990e
takahasinaoki@gmail.com <takanaweb5>
2021-02-06 21:55:46 +0900

    APIPass成功

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,30 @@
 import * as functions from "firebase-functions";
-import * as axios from "axios";
-import * as express from "express";
+import axios, { AxiosRequestConfig } from "axios";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.region("asia-northeast2").https.onRequest((req: express.Request, res: express.Response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-    res.send("Hello from Firebase!");
+async function main(req:any, res:any) {
+  try {
+    const config:AxiosRequestConfig = {
+      headers: {
+        'Content-Type': 'text/plain'
+      },
+      responseType: 'text'
+    };
+    const ret = await axios.post('http://takana.web5.jp/test/test.php',
+                                  'http://xxx.jp?name=sato&age=55' + '\t' +'名前=山田&年齢=99.99', config);
+    const text = ret.data;
+    console.log(text);
+    res.send(text);
+  } catch (error) {
+    const {
+      status,
+      statusText
+    } = error.response;
+    const msg = `Error! HTTP Status: ${status} ${statusText}`;
+    console.log(msg);
+    res.send(msg);
+  }
+};
+
+export const APIPass = functions.region("asia-northeast2").https.onRequest((req, res) => {
+  main(req, res);
 });
