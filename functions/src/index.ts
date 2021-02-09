76396f62bf1ef060624ef48872f354096ea6b6ba
takahasinaoki@gmail.com <takanaweb5>
2021-02-09 22:05:00 +0900

    ひな型

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,8 @@
 import * as functions from "firebase-functions";
-import axios, { AxiosRequestConfig } from "axios";
+import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 import * as express from "express"; //型情報のみ(index.jsでは削除される)
 
-async function callWebAPI(url:string, postMsg:string) {
+async function callWebAPI(url:string, postMsg:string): Promise<string> {
   try {
     const config:AxiosRequestConfig = {
       headers: {'Content-Type': 'text/plain'},
@@ -13,27 +13,28 @@ async function callWebAPI(url:string, postMsg:string) {
   } catch (error) {
     const {
       status,
-      statusText
-    } = error.response;
+      statusText,
+    } = error.response as AxiosResponse;
     throw `Error! HTTP Status: ${status} ${statusText}`;
   }
 };
 
 export const APIPass = functions.region("asia-northeast2").https.onRequest((request:express.Request, response:express.Response) => {
-  // const url = 'http://xxxxxxxx.jp/test.php'
+  // const url = 'http://xxxxxxxxx.jp/test/test.php'
   const url = process.env.url as string; //環境変数から取得
 
   //別ドメインからアクセスした場合のcorsエラーを回避する必要のない時は不要
-  // resp.set('Access-Control-Allow-Origin', '*'); //すべて許可
+  // response.set('Access-Control-Allow-Origin', '*'); //すべて許可
   response.set('Access-Control-Allow-Origin', process.env.url2); //設定したドメインのみ許可
 
   callWebAPI(url, request.body)
+    // .then(result => {
+    //   return callWebAPI(url, result);
+    // })
     .then(result => {
-      console.log(result);
       response.send(result);
     })
     .catch(errMsg => {
-      console.log(errMsg);
       response.send(errMsg);
     });
 });
